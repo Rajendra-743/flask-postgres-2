6462210256e402299289b34ada38eb899690d95e
GanesaAprilyanPhanama <126218565+GanesaAprilyanPhanama@users.noreply.github.com>
2023-09-21 14:23:28 +0700

    added sign up

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
 import psycopg2
+import bcrypt
+from flask import Flask,request, jsonify    
 from psycopg2 import sql
 
 app = Flask(__name__)
@@ -25,5 +26,29 @@ def hello_world():
     conn.close()
     return result
 
+@app.route('/signup', methods=['POST'])
+def signup():
+    data = request.json
+    full_name = data.get('full_name')
+    username = data.get('username')
+    password = data.get('password')
+
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        """
+        INSERT INTO users (full_name, username, password)
+        VALUES (%s, %s, %s)
+        """,
+        (full_name, username, hashed_password)
+    )
+    conn.commit()
+    conn.close()
+    
+    response = {'message': 'Signup successful'}
+    return jsonify(response), 201
+
 if __name__ ==  '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
